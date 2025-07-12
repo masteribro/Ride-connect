b067fb8b5ee206e1879c9e423ba66664deb64967
masteribro <mohammedibrahim075@gmail.com>
2025-07-12 23:05:05 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ride_connect/presentation/auth/login_page.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ride Connect',
+      home: const LoginPage(),
+    );
+  }
+}
+
+
